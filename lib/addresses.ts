a69b5fb0926084f41aa9afb9b728a13d283c9d56
Dameon Mendoza <dameonmendoza@gmail.com>
2022-04-06 11:36:25 -0700

    next export added for gh pages

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -3,4 +3,6 @@ import { PublicKey } from "@solana/web3.js"
 // Your shop wallet address
 export const shopAddress = new PublicKey('6iyb7Cv1vtmqmN6RysZoJoYXKJDESZdgFdTTpGKjzogJ');
 
-export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr');
+// This is your token/coupon address
+export const couponAddress = new PublicKey('AF38iwTUq4VEefv8X4skB9LMpXZiguuGgSsyxWqMD6Dd')
