711a315ce857990d65b2c98a417a2645d319e1e4
Dameon Mendoza <dameonmendoza@gmail.com>
2022-04-06 15:06:28 -0700

    updated contract owner

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -1,8 +1,8 @@
 import { PublicKey } from "@solana/web3.js"
 
 // Your shop wallet address
-export const shopAddress = new PublicKey('6iyb7Cv1vtmqmN6RysZoJoYXKJDESZdgFdTTpGKjzogJ');
+export const shopAddress = new PublicKey('3JF8JuGRsHtyFiuh4KgvCakHPVNCzfEMYAeHWmqrKCQm');
 
 export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr');
 // This is your token/coupon address
-export const couponAddress = new PublicKey('AF38iwTUq4VEefv8X4skB9LMpXZiguuGgSsyxWqMD6Dd')
+export const couponAddress = new PublicKey('C8deCQQtxNvVDvi3ho6V4in2aKu7Pyr6mFXydPRCurmi')
