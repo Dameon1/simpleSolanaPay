c79bcda3de07f77b42f44e53f7e2ef1e9d0f32c6
Dameon Mendoza <dameonmendoza@gmail.com>
2022-04-06 09:31:17 -0700

    changing dollars

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -1,4 +1,6 @@
 import { PublicKey } from "@solana/web3.js"
 
 // Your shop wallet address
-export const shopAddress = new PublicKey('6iyb7Cv1vtmqmN6RysZoJoYXKJDESZdgFdTTpGKjzogJ') 
+export const shopAddress = new PublicKey('6iyb7Cv1vtmqmN6RysZoJoYXKJDESZdgFdTTpGKjzogJ');
+
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
